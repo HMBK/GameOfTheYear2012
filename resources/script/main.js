744da8cb15b3bde495bbe9a78bb3dad57b280bd8
HeMightBeKilv <kilvastheninja@hotmail.com>
2015-12-13 08:51:35 -0500

    Colour!

--- resources/script/main.js
+++ resources/script/main.js
@@ -2,15 +2,17 @@
 /********
 NOTES
 {
-	use console.log() to debug
-	to disable a button such as $scrounge, use $('#scrounge').addClass('disabled');
-
+	-use console.log() to debug
+	-to disable a button such as $scrounge, use $('#scrounge').addClass('disabled');
+	-Save() / Load() could be a problem in the future if you change the order of things pushed into inv, so... don't?
 }
 ********/
 
 //initialize variables
 var gald = 0;
-var inv = []; //[name, #of, initCost, curCost]
+var inv = []; //[0 name, 1 #of, 2 initCost, 3 curCost]
+
+//adding purchasable buildings
 inv.push(["homeless", 0, 100, 100]);
 
 //main functions
@@ -48,22 +50,21 @@ function buyObject(object) //new cost = base cost(1.1^n) where n=num of object
 //Save game function
 function save()
 {
-	var save = //creates the variable, which stores information we want to save
-	{
-		gald: gald,
-		homeless: inv[0][1]
-	}
-	localStorage.setItem("save",JSON.stringify(save)); //JSON method turns info into a String
+	var saveFile = []; //creates the variable, which stores information we want to save
+	for(var i=0; i<inv.length; i++) //clones contents of inv into saveFile
+		saveFile.push(inv[i]);
+	saveFile.push(gald);
+	localStorage.setItem("saveFile",JSON.stringify(saveFile)); //JSON method turns info into a String
 }
 
 //Load game function
-function load()
+function load() //saveFile stores inv[i, i+1, i+2...] as [i, i+1, i+2...] and gald as the last entry
 {
-	var savegame = JSON.parse(localStorage.getItem("save")); //same as above but reverse
-	//above var should now contain info via savegame.gald, etc
-	if (typeof savegame.gald !== "undefined") gald = savegame.gald; //If a past save didn't have a new mechanic,
-	if (typeof savegame.homeless !== "undefined") inv[0][1] = savegame.homeless; //load it. Else, it'll start at 0.
-	inv[0][3] = Math.floor(inv[0][2] * Math.pow(1.1,inv[0][1])); //sets prices based on #of loaded; MAKE A FOR LOOP
+	var savegame = JSON.parse(localStorage.getItem("saveFile")); //decrypts String; converts into values via savegame.gald etc
+	gald=savegame[savegame.length-1]; //gald becomes last entry of savegame
+	for(var i=0; i<inv.length; i++) //runs for the current length of inv
+		for(var x=0; x<inv[i].length; x++) //runs for the length of array at i
+			inv[i][x] = savegame[i][x]; //each value at x in array i for inv becomes the matching value from savegame
 	console.log("Loaded!");
 	refresh();
 }
@@ -71,12 +72,11 @@ function load()
 //Erase save function
 function erase()
 {
-	localStorage.removeItem("save");
+	localStorage.removeItem("saveFile"); //removes the save from clientside storage
 	gald=0;
-		inv[0]=["homeless",0,100,100];//make this a for loop too; inv[i] at 0 & 2 can stay the same, 1="0" and 3=2
-		refresh();
-		console.log("Erased!");
-		return;
+	inv[0]=["homeless",0,100,100]; //make this a for loop too; inv[i] at 0 & 2 can stay the same, 1="0" and 3=2
+	refresh();
+	console.log("Erased!");
 }
 
 //Function to prettify numbers & rogue decimals
@@ -96,11 +96,16 @@ window.setInterval(function()
 }, 500); //The above fires once every 1000ms
 
 //THE SAVE LOOP
+/*
 window.setInterval(function()
 {
 	save(); 
 	console.log("Saved.");
 }, 30000); //The above fires once every 1000ms
+*/
+window.addEventListener("beforeunload", function() {
+    save();
+});
 
 //Refreshes displayed values in the HTML file
 function refresh()
